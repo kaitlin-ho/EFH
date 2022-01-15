b2b091bfc92b835ba2cc23d036750028baeb33cf
Kaitlin Ho <76082296+kaitlin-ho@users.noreply.github.com>
2022-01-15 17:27:41 -0500

    Update Monster.java

--- v0/Monster.java
+++ v0/Monster.java
@@ -1,10 +1,10 @@
 public class Monster extends Adversary{
 
 	public Monster() {
-		hp = 0; //these will be overriden in the sublevel monsters
-		strength = 0; //^
-		defense = 0;
-		attackRating = 1;
+		_hp = 0; //these will be overriden in the sublevel monsters
+		_strength = 0; //^
+		_defense = 0;
+		_attackRating = 1;
 	}
 
 }
