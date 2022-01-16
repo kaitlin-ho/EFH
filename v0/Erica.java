8f9ce9f7ae0d2ca3cb8b1b917919b7847f5acb7a
AriellaKatz <akatz30@stuy.edu>
2022-01-15 19:42:24 -0500

    npcs

--- v0/Erica.java
+++ v0/Erica.java
@@ -1,4 +1,5 @@
 public class Erica extends NPC{
 
-  
+	
+
 }
--- v0/NPC.java
+++ v0/NPC.java
@@ -1,4 +1,31 @@
 public class NPC extends Chatbox{
 
-  
+	protected int _rightAnsCt;
+	protected ArrayList<String> _questions;
+	protected ArrayList<String> _keywords;
+
+	public NPC() {
+		_rightAnsCt = 0;
+		_questions = new ArrayList<String>(2)
+		_keywords = new ArrayList<String>(2)
+	}
+
+	public String getResponse() {
+		String response = "Hello!";
+		if (_rightAnsCt == 0) {
+			response += " " + _questions.get(0);
+		}
+		else if (_rightAnsCt == 1) {
+			response += " " + _questions.get(1);
+		}
+		else {
+			response += " You have already answered all of my questions! " 
+			+ "Search elsewhere for a source of equipment."
+		}
+	}
+
+	public void judge(String answer) {
+
+	}
+
 }
