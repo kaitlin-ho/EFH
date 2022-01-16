3853e919cd212ba1c1d82db5ffdc334eaee240cb
Kaitlin Ho <76082296+kaitlin-ho@users.noreply.github.com>
2022-01-16 17:21:23 -0500

    Update Boss.java

--- v1/Boss.java
+++ v1/Boss.java
@@ -2,8 +2,8 @@ public class Boss extends Monster{
 
         public Boss() {
                 super();
-                _hp = 100;
-                _strength = 50;
+                _hp = 2500;
+                _strength = 200;
         }
 
 }
