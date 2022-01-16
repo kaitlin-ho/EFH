f323d359d048944e3ba276986e5faada2aee6094
Kaitlin Ho <76082296+kaitlin-ho@users.noreply.github.com>
2022-01-16 17:22:49 -0500

    Update Armor.java

--- v1/Armor.java
+++ v1/Armor.java
@@ -3,7 +3,7 @@ public class Armor extends Equipment{
   public Armor() {
     _name = "Armor";
     _strengthAlt = 10;
-    _defenseAlt = 10;
+    _defenseAlt = 50;
   }
 
   public String about() {
