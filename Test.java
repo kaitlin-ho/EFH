d57ae697c3c7ac4bf10b59995576baba5186a52c
Hugo Jenkins <hjenkins30@stuy.edu>
2022-01-18 12:30:08 -0500

    class progress

--- Test.java
+++ Test.java
@@ -2,12 +2,24 @@
 Erica's Fans and Hugo (EFH):
 Hugo Jenkins, Ariella Katz, Kaitlin Ho, Boary, Tom, Apple
 */
-
+import java.util.*;
 public class Test {
-
-	public static void main(String[] args) {
-		Player player = new Player();
-		System.out.println(player.isAlive());
+	private static void wait(int millis)
+	  {
+	    try {
+				Thread.sleep(millis);
+				System.out.println("times up");
+	    }
+	    catch (InterruptedException e) {
+	    }
 	}
 
+	public static void main(String[] args) {
+			System.out.println("hello: ");
+			wait(2000);
+			Scanner in = new Scanner(System.in);
+			String line = in.nextLine();
+
+
+	}
 }
