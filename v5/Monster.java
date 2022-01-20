6feb88bafc05ef780eb7d54522bdcba284b06363
Ariella Katz <90809602+AriellaKatz@users.noreply.github.com>
2022-01-20 17:58:16 -0500

    Update Monster.java

--- v5/Monster.java
+++ v5/Monster.java
@@ -6,6 +6,7 @@ Hugo Jenkins, Ariella Katz, Kaitlin Ho, Boary, Tom, Apple
 public class Monster extends Adversary{
 
 	public Monster() {
+		_lives = 1; //_lives will not change, but we want it to be >0 so that isAlive() still works
 		_hp = 100; //these will be overriden in the sublevel monsters
 		_strength = 50; //^
 		_defense = 50;
