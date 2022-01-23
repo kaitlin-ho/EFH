ece030a7d6f016646ae4991c21a94a7c328904c1
Ariella Katz <90809602+AriellaKatz@users.noreply.github.com>
2022-01-22 22:17:15 -0500

    Update Mykolyk.java

--- v6/Mykolyk.java
+++ v6/Mykolyk.java
@@ -7,12 +7,12 @@ import java.util.*;
 public class Mykolyk extends NPC{
   public Mykolyk(InvisCloak invisCloak, KtS kts) {
     super();
-    _questions.add("Question 1");
-    _questions.add("Question 2");
+    _questions.add("What is the Wikipedia definition of computer science? Start with \"Computer science is \".");
+    _questions.add("f(n) is \"Big Oh\" of g(n) if... (include spaces before and after any/all operators)");
     ArrayList<String> answersOne = new ArrayList<String>(1);
-    answersOne.add("answer 1");
+    answersOne.add("Computer science is the study of theoretical foundations of computation and of practical implementation of this theory in computer systems.");
     ArrayList<String> answersTwo = new ArrayList<String>(1);
-    answersTwo.add("answer 2");
+    answersTwo.add("f(n) <= C * g(n) for some C & k where n > k.");
     _keywords.add(answersOne);
     _keywords.add(answersTwo);
     _equipment.add(invisCloak);
