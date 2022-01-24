e6158ca356c6d7d6d81e973ebe290a181fe06a30
hugojenkins11 <hjenkins30@stuy.edu>
2022-01-23 22:35:52 -0500

    copy to root level

---
+++ v7/Equipment.java
@@ -0,0 +1,16 @@
+/*
+Erica's Fans and Hugo (EFH):
+Hugo Jenkins, Ariella Katz, Kaitlin Ho, Boary, Tom, Apple
+*/
+
+public class Equipment implements EqInt{
+
+	protected String _name;
+	protected int _strengthAlt;
+	protected int _defenseAlt;
+
+	public String about() {
+		return "Equipment modifies your strength and defense to help you defeat monsters.";
+	}
+
+}
